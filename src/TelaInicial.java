b3fb0a7a42709d85131bb34dac40266c099288cd
MariPadilha <lunaforever53@gmail.com>
2025-06-28 22:05:17 -0300

    versao 2

--- src/TelaInicial.java
+++ src/TelaInicial.java
@@ -9,21 +9,21 @@ public class TelaInicial extends JPanel{
 
     public TelaInicial(DeliveryMachine jogo){
         this.jogo = jogo;
-        jogo.gerarEnderecoVirtual();
-        criaBackground();
-        criaLetreiro();
-        criaBotao();
-    }
 
-    private void criaBackground(){
         setLayout(new BorderLayout());
         Dimension tamanhoTela = Toolkit.getDefaultToolkit().getScreenSize();
         setPreferredSize(tamanhoTela);
 
+        criaBackground(tamanhoTela);
+        criaLetreiro(tamanhoTela);
+        criaBotao(tamanhoTela);
+    }
+
+    private void criaBackground(Dimension tamanhoTela){
         backgroundImage = new ImageIcon("/home/mari/Development/project_java/delivery_machine/imagens/backgroundTelaInicial.png").getImage();
     }
 
-    private void criaBotao(){
+    private void criaBotao(Dimension tamanhoTela){
         ImageIcon playIcon = new ImageIcon("/home/mari/Development/project_java/delivery_machine/imagens/play.png");
         Image img = playIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
         Icon iconRedondo = new ImageIcon(img);
@@ -38,6 +38,8 @@ public class TelaInicial extends JPanel{
 
         botaoRedondo.addActionListener(e -> {
             JOptionPane.showMessageDialog(null, "endereco virtual gerado:" + jogo.getEnderecoVirtual());
+            jogo.gerarEnderecoVirtual();
+            jogo.gerarEnderecoFisico();
             jogo.mostrarTela("tlb");
         });
 
@@ -47,45 +49,57 @@ public class TelaInicial extends JPanel{
         add(painelCentral, BorderLayout.CENTER);
     }
 
-    private void criaLetreiro(){
-        Font fontePersonalizada = null;
+    private void criaLetreiro(Dimension tamanhoTela){
+        float tamanhoFonte = tamanhoTela.height * 0.12f;
+        int deslocamentoX = tamanhoTela.width / 200;
 
+        Font fonte = carregarFontePersonalizada(
+            "/home/mari/Development/project_java/delivery_machine/fonte/VT323-Regular.ttf",
+            tamanhoFonte
+        );
+
+        JLabel titulo = aplicarSombraTexto("▶ DELIVERY MACHINE ◀", fonte, 
+            new Color(139, 0, 0, 180), new Color(200, 111, 58), deslocamentoX);
+
+        int margemTopo = (int)(tamanhoTela.height * 0.15);
+        titulo.setBorder(BorderFactory.createEmptyBorder(margemTopo, 0, 0, 0));
+        titulo.setPreferredSize(new Dimension(tamanhoTela.width, (int)tamanhoFonte * 2));
+
+        add(titulo, BorderLayout.NORTH);
+    }
+
+    private Font carregarFontePersonalizada(String caminho, float tamanho){
         try{
-            fontePersonalizada = Font.createFont(Font.TRUETYPE_FONT,
-                new File("/home/mari/Development/project_java/delivery_machine/fonte/VT323-Regular.ttf")).deriveFont(Font.BOLD, 300f);
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(fontePersonalizada);
+            Font fonte = Font.createFont(Font.TRUETYPE_FONT, new File(caminho)).deriveFont(Font.BOLD, tamanho);
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(fonte);
+            return fonte;
         }catch(IOException | FontFormatException e){
             e.printStackTrace();
-            fontePersonalizada = new Font("Monospaced", Font.BOLD, 300);
+            return new Font("Monospaced", Font.BOLD, (int)tamanho);
         }
+    }
 
-        JLabel titulo = new JLabel("▶ DELIVERY MACHINE ◀", SwingConstants.CENTER) {
+    private JLabel aplicarSombraTexto(String texto, Font fonte, Color corSombra, Color corTexto, int deslocamentoX){
+        return new JLabel(texto, SwingConstants.CENTER){
             @Override
-            protected void paintComponent(Graphics g) {
-                Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            protected void paintComponent(Graphics g){
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-                String texto = getText();
-                FontMetrics fm = g2d.getFontMetrics(getFont());
-                int textoLargura = fm.stringWidth(texto);
-                int x = (getWidth() - textoLargura) / 2;
+                g2.setFont(fonte);
+                FontMetrics fm = g2.getFontMetrics();
+                int x = (getWidth() - fm.stringWidth(texto)) / 2;
                 int y = getBaseline(getWidth(), getHeight());
 
-                g2d.setColor(new Color(139, 0, 0, 180));
-                g2d.drawString(texto, x + 10, y);
+                g2.setColor(corSombra);
+                g2.drawString(texto, x + deslocamentoX, y);
 
-                g2d.setColor(new Color(200, 111, 58));
-                g2d.drawString(texto, x, y);
+                g2.setColor(corTexto);
+                g2.drawString(texto, x, y);
 
-                g2d.dispose();
+                g2.dispose();
             }
         };
-
-        titulo.setFont(fontePersonalizada);
-        titulo.setOpaque(false);
-        titulo.setBorder(BorderFactory.createEmptyBorder(150, 0, 0, 0));
-        add(titulo, BorderLayout.NORTH);
     }
 
     @Override
